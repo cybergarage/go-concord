5549d33c7c61a6ce3523f9a1d6fac027a279c057
Satoshi Konno <skonno@cybergarage.org>
2025-12-30 22:43:47 +0900

    feat(coordinator): add internale store constants

---
+++ concord/coordinator/store.go
@@ -0,0 +1,23 @@
+// Copyright (C) 2025 The Concord Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package coordinator
+
+const (
+	createdAtKey      = "_created_at"
+	updatedAtKey      = "_updated_at"
+	createRevisionKey = "_create_revision"
+	modRevisionKey    = "_mod_revision"
+	versionKey        = "_version"
+)
